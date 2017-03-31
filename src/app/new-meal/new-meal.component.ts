972699799a41ad5f0ca5393412feb77cdbeb63fb
stella huayhuaca <luces.huayhuaca@gmail.com>
2017-03-31 13:14:27 -0700

    display new-meal in template

--- src/app/new-meal/new-meal.component.ts
+++ src/app/new-meal/new-meal.component.ts
@@ -7,9 +7,12 @@ import { Meal } from './../meal.model';
   styleUrls: ['./new-meal.component.css']
 })
 export class NewMealComponent implements OnInit {
-
+  @Output() newMealSender = new EventEmitter();
+  submitForm(name:string, calorie:number, details:string, image:string){
+    var newMeal: Meal = new Meal(name, calorie, details, image);
+    this.newMealSender.emit(newMeal);
+  }
   constructor() { }
-
   ngOnInit() {
   }
 
